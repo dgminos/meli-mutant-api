d21a30ce9aa0baff2b5662a7208d71d4dcc74042
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2024-11-11 22:13:25 -0300

    remove path to .env file

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function hasConsecutiveLetters(sequence) {
   return false
 }
 // Endpoint POST /mutant
-process.loadEnvFile("./.env")
+process.loadEnvFile()
 const restApiURL = process.env.REST_API_PUBLIC_URL
 
 app.post(restApiURL+"/mutant/", (req, res) => {
